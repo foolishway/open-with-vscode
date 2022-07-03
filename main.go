022757f61139d35ea3b7bb56b815a3c05f19fdfb
zhangwei412827@163.com <zhangwei25@kuaishou.com>
2022-07-03 16:04:30 +0800

    chore: refactor

--- main.go
+++ main.go
@@ -22,12 +22,10 @@ func main() {
 		openWithVscode(t)
 	} else {
 		var gitRepo = *source
-
-		var dirName string = (gitRepo)[strings.LastIndex((gitRepo), "/")+1:]
-		dirName = dirName[:strings.LastIndex(dirName, ".")]
-
 		var clonePath = ""
 		if len(flag.Args()) != 0 {
+			var dirName string = (gitRepo)[strings.LastIndex((gitRepo), "/")+1:]
+			dirName = dirName[:strings.LastIndex(dirName, ".")]
 			clonePath = path.Clean(flag.Args()[0] + "/" + dirName)
 		}
 		gitClone(gitRepo, clonePath)
